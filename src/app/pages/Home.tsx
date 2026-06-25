623576506e2b149c5525a152fb6982a527373c2d
alvi2693 <jorge.alvaro.cc@gmail.com>
2026-06-25 15:30:53 +0200

    fix JSX tag structure in Home

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -232,6 +232,7 @@ export function Home() {
                       ))}
                     </div>
                   </div>
+                </div>
                 </Link>
               </motion.div>
             ))}
